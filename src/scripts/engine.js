c8a86d9265e16dba16f7901a6c5b2823d16b7c19
Steph Hoel <steph.hoel@gmail.com>
2023-10-01 20:24:43 -0300

    feat: add audio bg

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -169,6 +169,10 @@ async function playAudio(status) {
 function init (){
   drawCards(5, state.playerSides.player1)
   drawCards(5, state.playerSides.computer)
+
+  const bgm = document.getElementById('bgm')
+  bgm.volume = 0.05
+  bgm.play()
 }
 
 init()
